cc5054fae823d048dd3d648c0750197f559aab34
Szymon Blaszke <79770851+Szym0neKkQQ@users.noreply.github.com>
2021-11-05 23:35:43 +0100

    Add files via upload

---
+++ Bubblesort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+
+            for (int i=0; i < 5 ;i++)
+            {
+                try
+                {
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Int Only");
+                    Program.Main(args);
+                }
+            }
+
+            for (int x=0; x<array.Length; x++)
+            {
+                for (int j=0; j<array.Length; j++)
+                {
+                    if (array[x]<array[j])
+                    {
+                        int bubble = array[x];
+                        array[x] = array[j];
+                        array[j] = bubble;
+                    }
+                }
+            }
+            Console.WriteLine(" ");
+            for (int i=0;i<array.Length; i++)
+            {
+                Console.WriteLine(array[i]+" ");
+            }
+
+        }
+
+        
+    }
+    
+}
